bf0a8e2c0d1a432b63e687e2756c1bb1d2d3bf99
nikhil <kumarnikil220@gmail.com>
2024-02-25 16:24:03 +0530

    Record inserted to database using jdbc program

--- src/ConnectDb.java
+++ src/ConnectDb.java
@@ -1,8 +1,11 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;;
+import java.sql.SQLException;
+import java.sql.PreparedStatement;
 
 public class ConnectDb {
+    public static Connection con=null;
+    public static PreparedStatement ps=null;
     public static void main(String[] args) {
         // Database URL
         String url = "jdbc:mysql://localhost:3306/students";
@@ -13,9 +16,16 @@ public class ConnectDb {
 
         // Establish the connection
         try {
-            Connection con = DriverManager.getConnection(url, username, password);
+            con = DriverManager.getConnection(url, username, password);
             System.out.println("Connected to database successfully");
             System.out.println(con);  //it will print the connection id
+            ps=con.prepareStatement("insert into students values(001,'nikhil','dhampur')");            
+            int i=ps.executeUpdate();
+            if(i>=1){
+                System.out.println("Record Inserted");
+            }else{
+                System.out.println("Try again");
+            }
          } catch (SQLException e) {
             System.err.println("Connection failed:" + e.getMessage());
         }
